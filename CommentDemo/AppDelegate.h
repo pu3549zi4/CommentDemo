880bfdab69813a4ec68af50fc62d05e40a8b9ea8
pu3549zi4 <xmart@xmartdemac-mini.local>
2016-08-11 11:58:55 +0800

    Initial Commit

---
+++ CommentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommentDemo
+//
+//  Created by Xmart on 16/8/11.
+//  Copyright © 2016年 Paul.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommentDemo
+//
+//  Created by Xmart on 16/8/11.
+//  Copyright © 2016年 Paul.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
